b7f328bba600b1c8d6a83407fc031f887ddd7938
asytyi <alexander.a.sytyi@gmail.com>
2020-07-01 18:40:47 +0300

    Разрешение работы WatchDog в файле Main.c

--- Firmware/Sources/Main.c
+++ Firmware/Sources/Main.c
@@ -14,7 +14,6 @@
 #include "InterboardProtocol.h"
 #include "SecondarySampling.h"
 
-
 // FORWARD FUNCTIONS
 // -----------------------------------------
 Boolean InitializeCPU();
@@ -51,63 +50,63 @@ ISRCALL IllegalInstruction_ISR();
 void main()
 {
 	Int16U i;
-
+	
 	// Boot process
 	InitializeCPU();
-
+	
 	// Switch GPIO in proper state
 	InitializeBoard();
-
-   	// Wait for power-on
-   	for(i = 0; i < MSC_PON_DELAY_MS; ++i)
-   	{
-   		DELAY_US(1000);
-//   		ZbWatchDog_Strobe();
-   	}
-
+	
+	// Wait for power-on
+	for(i = 0; i < MSC_PON_DELAY_MS; ++i)
+	{
+		DELAY_US(1000);
+		ZbWatchDog_Strobe();
+	}
+	
 	// Only if good clocking was established
 	InitializeTimers();
 	InitializeADC();
 	InitializeSPI();
 	InitializeSCI();
 	InitializeCAN();
-
+	
 	// Setup ISRs
 	BEGIN_ISR_MAP
-		ADD_ISR(TINT0, Timer0_ISR);
-		ADD_ISR(TINT2, Timer2_ISR);
-		ADD_ISR(ECAN0INTA, CAN0_ISR);
-		ADD_ISR(SEQ1INT, SEQ1_ISR);
-		ADD_ISR(EPWM3_TZINT, PWM3_TZ_ISR);
-		ADD_ISR(SPIRXINTA, SPIaRX_ISR);
-		ADD_ISR(ILLEGAL, IllegalInstruction_ISR);
+	ADD_ISR(TINT0, Timer0_ISR);
+	ADD_ISR(TINT2, Timer2_ISR);
+	ADD_ISR(ECAN0INTA, CAN0_ISR);
+	ADD_ISR(SEQ1INT, SEQ1_ISR);
+	ADD_ISR(EPWM3_TZINT, PWM3_TZ_ISR);
+	ADD_ISR(SPIRXINTA, SPIaRX_ISR);
+	ADD_ISR(ILLEGAL, IllegalInstruction_ISR);
 	END_ISR_MAP
-
+	
 	// Init board external watch-dog
-//   	ZbWatchDog_Init();
-
-   	// Initialize controller logic
+	ZbWatchDog_Init();
+	
+	// Initialize controller logic
 	InitializeController();
-
+	
 	// Enable interrupts
 	EINT;
 	ERTM;
-
+	
 	// Do delayed initialization
 	CONTROL_DelayedInit();
-
+	
 	// Set watch-dog as WDRST
-//	ZwSystem_SelectDogFunc(FALSE);
-//	ZwSystem_EnableDog(SYS_WD_PRESCALER);
-//	ZwSystem_LockDog();
-
+	ZwSystem_SelectDogFunc(FALSE);
+	ZwSystem_EnableDog(SYS_WD_PRESCALER);
+	ZwSystem_LockDog();
+	
 	// Start timers
 	ZwTimer_StartT2();
-
+	
 	// Re-init SPI
 	DELAY_US(MSC_PON_SPI_RST * 1000L);
 	CONTROL_ReInitSPI_Rx();
-
+	
 	// Background cycle
 	while(TRUE)
 		CONTROL_Idle();
@@ -117,38 +116,38 @@ void main()
 // Initialize and prepare DSP
 Boolean InitializeCPU()
 {
-    Boolean clockInitResult;
-
+	Boolean clockInitResult;
+	
 	// Init clock and peripherals
-    clockInitResult = ZwSystem_Init(CPU_PLL, CPU_CLKINDIV, SYS_LOSPCP, SYS_HISPCP, SYS_PUMOD);
-
-    if(clockInitResult)
-    {
+	clockInitResult = ZwSystem_Init(CPU_PLL, CPU_CLKINDIV, SYS_LOSPCP, SYS_HISPCP, SYS_PUMOD);
+	
+	if(clockInitResult)
+	{
 		// Do default GPIO configuration
 		ZwGPIO_Init(GPIO_TSAMPLE, GPIO_TSAMPLE, GPIO_TSAMPLE, GPIO_TSAMPLE, GPIO_TSAMPLE);
 		// Initialize PIE
 		ZwPIE_Init();
 		// Prepare PIE vectors
 		ZwPIE_Prepare();
-    }
-
+	}
+	
 	// Configure flash
 	ZW_FLASH_CODE_SHADOW;
 	ZW_FLASH_MATH_SHADOW;
 	ZW_FLASH_OPTIMIZE(FLASH_FWAIT, FLASH_OTPWAIT);
-
-   	return clockInitResult;
+	
+	return clockInitResult;
 }
 // -----------------------------------------
 
 // Initialize CPU timers
 void InitializeTimers()
 {
-    ZwTimer_InitT0();
+	ZwTimer_InitT0();
 	ZwTimer_SetT0(TIMER0_PERIOD);
 	ZwTimer_EnableInterruptsT0(TRUE);
-
-    ZwTimer_InitT2();
+	
+	ZwTimer_InitT2();
 	ZwTimer_SetT2(TIMER2_PERIOD);
 	ZwTimer_EnableInterruptsT2(TRUE);
 }
@@ -159,7 +158,7 @@ void InitializeADC()
 	// Initialize and prepare ADC
 	ZwADC_Init(ADC_PRESCALER, ADC_CD2, ADC_SH);
 	ZwADC_ConfigInterrupts(TRUE, FALSE);
-
+	
 	// Enable interrupts on peripheral and CPU levels
 	ZwADC_EnableInterrupts(TRUE, FALSE);
 	ZwADC_EnableInterruptsGlobal(TRUE);
@@ -172,7 +171,7 @@ void InitializeSCI()
 	ZwSCIa_Init(SCIA_BR, SCIA_DB, SCIA_PARITY, SCIA_SB, FALSE);
 	ZwSCIa_InitFIFO(16, 0);
 	ZwSCIa_EnableInterrupts(FALSE, FALSE);
-
+	
 	ZwSCI_EnableInterruptsGlobal(FALSE);
 }
 // -----------------------------------------
@@ -184,13 +183,13 @@ void InitializeSPI()
 	ZwSPIb_InitFIFO(0, 0);
 	ZwSPIb_ConfigInterrupts(FALSE, FALSE);
 	ZwSPIb_EnableInterrupts(FALSE, FALSE);
-
+	
 	// Init master optical receiver interface
 	ZwSPIa_Init(FALSE, 0, 16, SPIA_PLR, SPIA_PHASE, ZW_SPI_INIT_RX, FALSE, FALSE);
 	ZwSPIa_InitFIFO(IBP_PACKET_SIZE, 0);
 	ZwSPIa_ConfigInterrupts(TRUE, FALSE);
 	ZwSPIa_EnableInterrupts(TRUE, FALSE);
-
+	
 	// Common (ABCD)
 	ZwSPI_EnableInterruptsGlobal(TRUE);
 }
@@ -200,22 +199,22 @@ void InitializeCAN()
 {
 	// Init CAN
 	ZwCANa_Init(CANA_BR, CANA_BRP, CANA_TSEG1, CANA_TSEG2, CANA_SJW);
-
+	
 	// Register system handler
 	ZwCANa_RegisterSysEventHandler(&CONTROL_NotifyCANFault);
-
-    // Allow interrupts for CAN
-    ZwCANa_InitInterrupts(TRUE);
-    ZwCANa_EnableInterrupts(TRUE);
+	
+	// Allow interrupts for CAN
+	ZwCANa_InitInterrupts(TRUE);
+	ZwCANa_EnableInterrupts(TRUE);
 }
 // -----------------------------------------
 
 void InitializeBoard()
 {
 	// Init board GPIO
-   	ZbGPIO_Init();
-   	// Init EPROM & SRAM
-   	ZbMemory_Init();
+	ZbGPIO_Init();
+	// Init EPROM & SRAM
+	ZbMemory_Init();
 }
 // -----------------------------------------
 
@@ -228,13 +227,13 @@ void InitializeController()
 // ISRs
 // -----------------------------------------
 #ifdef BOOT_FROM_FLASH
-	#pragma CODE_SECTION(Timer0_ISR, "ramfuncs");
-	#pragma CODE_SECTION(Timer2_ISR, "ramfuncs");
-	#pragma CODE_SECTION(CAN0_ISR, "ramfuncs");
-	#pragma CODE_SECTION(SEQ1_ISR, "ramfuncs");
-	#pragma CODE_SECTION(PWM3_TZ_ISR, "ramfuncs");
-	#pragma CODE_SECTION(SPIaRX_ISR, "ramfuncs");
-	#pragma CODE_SECTION(IllegalInstruction_ISR, "ramfuncs");
+#pragma CODE_SECTION(Timer0_ISR, "ramfuncs");
+#pragma CODE_SECTION(Timer2_ISR, "ramfuncs");
+#pragma CODE_SECTION(CAN0_ISR, "ramfuncs");
+#pragma CODE_SECTION(SEQ1_ISR, "ramfuncs");
+#pragma CODE_SECTION(PWM3_TZ_ISR, "ramfuncs");
+#pragma CODE_SECTION(SPIaRX_ISR, "ramfuncs");
+#pragma CODE_SECTION(IllegalInstruction_ISR, "ramfuncs");
 #endif
 //
 #pragma INTERRUPT(Timer0_ISR, HPI);
@@ -243,10 +242,10 @@ ISRCALL Timer0_ISR(void)
 {
 	// Do control cycle
 	CONTROL_RealTimeCycle();
-
+	
 	// Handle IBP timeouts
 	IBP_HighSpeedTimeoutCycle();
-
+	
 	// allow other interrupts from group 1
 	TIMER0_ISR_DONE;
 }
@@ -255,20 +254,20 @@ ISRCALL Timer0_ISR(void)
 ISRCALL Timer2_ISR(void)
 {
 	static Int16U dbgCounter = 0;
-
+	
 	// Update low-priority tasks
 	CONTROL_UpdateLow();
-
+	
 	// Update time
 	++CONTROL_TimeCounter;
-
+	
 	// Service watch-dogs
-	if (CONTROL_BootLoaderRequest != BOOT_LOADER_REQUEST)
+	if(CONTROL_BootLoaderRequest != BOOT_LOADER_REQUEST)
 	{
 		ZwSystem_ServiceDog();
-//		ZbWatchDog_Strobe();
+		ZbWatchDog_Strobe();
 	}
-
+	
 	++dbgCounter;
 	if(dbgCounter == DBG_COUNTER_PERIOD)
 	{
@@ -288,7 +287,7 @@ ISRCALL SEQ1_ISR(void)
 	ZwADC_ProcessInterruptSEQ1();
 	// Dispatch results
 	ZwADC_Dispatch1();
-
+	
 	// allow other interrupts from group 1
 	ADC_ISR_DONE;
 }
@@ -297,7 +296,7 @@ ISRCALL SEQ1_ISR(void)
 // Line 0 ISR
 ISRCALL CAN0_ISR(void)
 {
-    // handle CAN system events
+	// handle CAN system events
 	ZwCANa_DispatchSysEvent();
 	// allow other interrupts from group 9
 	CAN_ISR_DONE;
@@ -308,16 +307,16 @@ ISRCALL CAN0_ISR(void)
 ISRCALL PWM3_TZ_ISR(void)
 {
 	DINT;
-
+	
 	// Shutdown bridge
 	ZwPWMB_SetValue12(0);
 	// Notify controller
 	CONTROL_RequestStop(DF_BRIDGE_SHORT, TRUE);
 	ZwPWM3_ProcessTZInterrupt();
-
+	
 	// allow other interrupts from group 2
 	PWM_TZ_ISR_DONE;
-
+	
 	EINT;
 }
 // -----------------------------------------
@@ -327,7 +326,7 @@ ISRCALL SPIaRX_ISR()
 	// Handle interrupt
 	ZwSPIa_ProcessRXInterrupt();
 	SS_HandleSlaveTransmission();
-
+	
 	// allow other interrupts from group 6
 	SPI_ISR_DONE;
 }
@@ -338,11 +337,11 @@ ISRCALL IllegalInstruction_ISR(void)
 {
 	// Disable interrupts
 	DINT;
-
+	
 	// Terminate PWM - force low
 	EPwm1Regs.AQCSFRC.bit.CSFA = 1;
 	EPwm2Regs.AQCSFRC.bit.CSFA = 1;
-
+	
 	// Reset system using WD
 	ZwSystem_ForceDog();
 }
