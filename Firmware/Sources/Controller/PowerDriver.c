df54e98599cfbaaee1aee5facb1ba355369323d5
asytyi <alexander.a.sytyi@gmail.com>
2020-07-15 11:34:18 +0300

    Реализовано отключение маломощного MW при работе мощных

--- Firmware/Sources/Controller/PowerDriver.c
+++ Firmware/Sources/Controller/PowerDriver.c
@@ -21,17 +21,18 @@ void DRIVER_Init()
 	// Enable TZ
 	ZwPWM_SetTZPullup(PFDisable, PFDontcare, PFDontcare, PFDontcare, PFDontcare, PFDontcare);
 	ZwPWM_ConfigTZ1(TRUE, PQ_Sample6);
-
+	
 	// Post TZ init delay
 	DELAY_US(1000);
-
+	
 	// Init PWM outputs
 	ZwPWMB_InitBridgeA12(CPU_FRQ, PWM_FREQUENCY, TZ_MASK_CBC_BRIDGE, TZ_MASK_OST_BRIDGE, PWM_SATURATION);
-	ZwPWM3_Init(PWMUp, CPU_FRQ, PWM_FREQUENCY, FALSE, FALSE, TZ_MASK_CBC_BRIDGE, TZ_MASK_OST_BRIDGE, TRUE, TRUE, TRUE, FALSE, FALSE);
-
+	ZwPWM3_Init(PWMUp, CPU_FRQ, PWM_FREQUENCY, FALSE, FALSE, TZ_MASK_CBC_BRIDGE, TZ_MASK_OST_BRIDGE, TRUE, TRUE, TRUE,
+			FALSE, FALSE);
+	
 	// Clear possible faults
 	DRIVER_ClearTZFault();
-
+	
 	// Configure TZ interrupts
 	ZwPWM_ConfigTZIntOST(FALSE, FALSE, DBG_USE_BRIDGE_SHORT, FALSE, FALSE, FALSE);
 }
@@ -49,10 +50,10 @@ void DRIVER_SwitchPowerHigh()
 {
 	// Отключение разряда
 	ZwGPIO_WritePin(PIN_DIS, TRUE);
-
+	
 	// Включение силовых БП
-   	ZwGPIO_WritePin(PIN_POWER_1, TRUE);
-   	ZwGPIO_WritePin(PIN_POWER_2, TRUE);
+	ZwGPIO_WritePin(PIN_POWER_1, FALSE);
+	ZwGPIO_WritePin(PIN_POWER_2, TRUE);
 	ZwGPIO_WritePin(PIN_POWER_3, TRUE);
 	ZwGPIO_WritePin(PIN_POWER_4, TRUE);
 }
@@ -62,10 +63,10 @@ void DRIVER_SwitchPowerLow()
 {
 	// Отключение разряда
 	ZwGPIO_WritePin(PIN_DIS, TRUE);
-
+	
 	// Включение низковольтного силового БП
-   	ZwGPIO_WritePin(PIN_POWER_1, TRUE);
-   	ZwGPIO_WritePin(PIN_POWER_2, FALSE);
+	ZwGPIO_WritePin(PIN_POWER_1, TRUE);
+	ZwGPIO_WritePin(PIN_POWER_2, FALSE);
 	ZwGPIO_WritePin(PIN_POWER_3, FALSE);
 	ZwGPIO_WritePin(PIN_POWER_4, FALSE);
 }
@@ -75,10 +76,10 @@ void DRIVER_SwitchPowerOff()
 {
 	// Включение разряда
 	ZwGPIO_WritePin(PIN_DIS, FALSE);
-
+	
 	// Отключение силовых БП
-   	ZwGPIO_WritePin(PIN_POWER_1, FALSE);
-   	ZwGPIO_WritePin(PIN_POWER_2, FALSE);
+	ZwGPIO_WritePin(PIN_POWER_1, FALSE);
+	ZwGPIO_WritePin(PIN_POWER_2, FALSE);
 	ZwGPIO_WritePin(PIN_POWER_3, FALSE);
 	ZwGPIO_WritePin(PIN_POWER_4, FALSE);
 }
