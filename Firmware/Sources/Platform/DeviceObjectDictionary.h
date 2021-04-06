7a0f1b477047cf9db750b5dfbaf48c795e88515b
asytyi <alexander.a.sytyi@gmail.com>
2021-04-06 17:22:11 +0300

    Реализована корректировка точки сэмплинга на плоском синусе

--- Firmware/Sources/Platform/DeviceObjectDictionary.h
+++ Firmware/Sources/Platform/DeviceObjectDictionary.h
@@ -83,7 +83,8 @@
 #define REG_POWER_OPT_COUNT			85	// Actual count of power options
 #define REG_MODIFY_SINE				86	// Enable sine modification at low currents
 #define REG_SKIP_NEG_LOGGING		87	// Skip negative pulses logging
-// 88 - 95
+#define REG_MODIFY_SINE_SHIFT		88	// Modified sine sample point shift from pwm peak (in ticks)
+// 89 - 95
 #define REG_SCURRENT1_FINE_P2		96	// Secondary current 1 tune quadratic coefficient P2 x1e6
 #define REG_SCURRENT1_FINE_P1		97	// Secondary current 1 tune quadratic coefficient P1 x1000
 #define REG_SCURRENT2_FINE_P2		98	// Secondary current 2 tune quadratic coefficient P2 x1e6
