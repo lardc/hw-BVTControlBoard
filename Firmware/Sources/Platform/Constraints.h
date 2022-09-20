17d6c8d96f51ec6db1a7830e4b2d49e3ac2ae719
asytyi <alexander.a.sytyi@gmail.com>
2022-09-20 12:26:52 +0300

    Актуализированы Constraints

--- Firmware/Sources/Platform/Constraints.h
+++ Firmware/Sources/Platform/Constraints.h
@@ -20,80 +20,69 @@ typedef struct __TableItemConstraint
 	Int16U Default;
 } TableItemConstraint;
 
-
 // Restrictions
 //
-#define TEST_CURRENT_MIN		1		// in mA x10
-#define TEST_CURRENT_MAX		3000	// in mA x10
-#define TEST_CURRENT_DEF		50		// in mA x10
-//
-#define LIMIT_VOLTAGE_MIN		100		// (in V)
-#define LIMIT_VOLTAGE_MAX		8000	// (in V)
-#define LIMIT_VOLTAGE_DEF		500		// (in V)
-//
-#define VPLATE_TIME_MIN			100		// (in ms)
-#define VPLATE_TIME_MAX			60000	// (in ms)
-#define VPLATE_TIME_DEF			500		// (in ms)
-//
-#define RATE_VAC_MIN			1		// (x100 V/s)
-#define RATE_VAC_MAX			100		// (x100 V/s)
-#define RATE_VAC_DEF			20		// (x100 V/s)
-//
-#define START_VAC_MIN			LIMIT_VOLTAGE_MIN
-#define START_VAC_MAX			5000	// (in V)
-#define START_VAC_DEF			500		// (in V)
-//
-#define VOLTAGE_FREQUENCY_MIN	50		// (in Hz)
-#define VOLTAGE_FREQUENCY_MAX	60		// (in Hz)
-#define VOLTAGE_FREQUENCY_DEF	50		// (in Hz)
-//
-#define BRAKE_TIME_MIN			1		// (in ms)
-#define BRAKE_TIME_MAX			1000	// (in ms)
-#define BRAKE_TIME_DEF			10		// (in ms)
-//
-#define TRANSFORMER_COFF_MIN	10		// 10:1
-#define TRANSFORMER_COFF_MAX	200		// 200:1
-#define TRANSFORMER_COFF_DEF	100		// 100:1
-//
-#define NOMINAL_PRIMARY_V_MIN	10		// (in V)
-#define NOMINAL_PRIMARY_V_MAX	400		// (in V)
-#define NOMINAL_PRIMARY_V_DEF	150		// (in V)
-//
-#define OPTO_MON_MIN			0
-#define OPTO_MON_MAX			50
-#define OPTO_MON_DEF			10
-//
-#define VOLTAGE_DIV_MIN			1		// Pulse rate 1:1
-#define VOLTAGE_DIV_MAX			100		// Pulse rate 1:100
-#define VOLTAGE_DIV_DEF			1		// Pulse rate 1:1
-//
+// in Vrms
+#define START_VOLTAGE_MIN		100
+#define START_VOLTAGE_MAX		1000
+#define START_VOLTAGE_DEF		500
+
+// in kV/sec x10
+#define VOLTAGE_RATE_MIN		1
+#define VOLTAGE_RATE_MAX		30
+#define VOLTAGE_RATE_DEF		10
+
+#define K_REG_MAX				100
+
+// in Vrms
+#define FE_ABS_MAX				1000
+#define FE_ABS_DEF				100
+
+// in %
+#define FE_REL_MAX				100
+#define FE_REL_DEF				10
+
+#define FE_COUNTER_MAX			10
+#define FE_COUNTER_DEF			3
+
+// in X:1
+#define TRANS_COEFF_MIN			50
+#define TRANS_COEFF_MAX			150
+#define TRANS_COEFF_DEF			100
+
+// in Vdc
+#define PRIM_VOLTAGE_MIN		1
+#define PRIM_VOLTAGE_MAX		200
+#define PRIM_VOLTAGE_DEF		150
+
+// in Vrms
+#define TARGET_VOLTAGE_MIN		1000
+#define TARGET_VOLTAGE_MAX		10500
+#define TARGET_VOLTAGE_DEF		1000
+
+// in Irms mA part
+#define ILIMIT_mA_MAX			100
+#define ILIMIT_mA_DEF			10
+
+// in Irms uA part
+#define ILIMIT_uA_MAX			999
+
+// in sec
+#define VOLTAGE_PLATE_TIME_MIN	1
+#define VOLTAGE_PLATE_TIME_MAX	60
+#define VOLTAGE_PLATE_TIME_DEF	1
+
 #define SCOPE_RATE_MAX			100		// skip 99 point, write 1
 #define SCOPE_RATE_DEF			0		// write all
-//
-#define LAST_FRAG_SIZE_DEF		1200
-//
-#define VPEAK_DETECT_MIN		95		// in %
-#define VPEAK_DETECT_MAX		100		// in %
-#define VPEAK_DETECT_DEF		98		// in %
-//
+
 #define X_D_DEF0				10
 #define X_D_DEF1				100
 #define X_D_DEF2				1000
 #define X_D_DEF3				10000
-//
-// Relatively to ADC scale
-#define CAP_V_COFF_N_DEF		48		// 48 / 1000 = 0.048
-#define	SCURRENT1_COFF_N_DEF	765		// (765 / 1000) / 100 = 0.00765
-#define	SCURRENT2_COFF_N_DEF	812		// 812 / 10000 = 0.0812
-#define	SCURRENT3_COFF_N_DEF	1000
-#define	SVOLTAGE1_COFF_N_DEF	183		//  183 / 1000 = 0.183
-#define	SVOLTAGE2_COFF_N_DEF	2015	// 2015 / 1000 = 2.015
-
 
 // Variables
 //
 extern const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE];
 extern const TableItemConstraint VConstraint[DATA_TABLE_WP_START - DATA_TABLE_WR_START];
-
 
 #endif // __CONSTRAINTS_H
